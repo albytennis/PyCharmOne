ab2a9c60a9a564f886371be5483b852a38663401
albytennis <alexrobkin@gmail.com>
2020-04-18 13:19:09 -0400

    Vinnys addition

--- 01-program.py
+++ 01-program.py
@@ -1,4 +1,6 @@
 #01-program.py
+#trying back and forth with Git Hub
+#If at first you don't succeed try try again
 number_one = int(input('Enter first number '))
 number_two = int(input('Enter second number '))
 print('Calculating Result ....')
---
+++ vinny.py
@@ -0,0 +1,43 @@
+body_part1 = input("Enter a body part ")
+adjective1 = input("Enter an adjective ")
+noun1 = input("Give me a sexy noun ")
+body_part2 = input("Give me another body part ")
+noun2 = input("Give me another noun ")
+body_part3 = input("Enter a body part ")
+verb1 = input("Verb ending in s ")
+body_part4 = input("Give another body part ")
+body_part5 = input("Give yet another body part ")
+verb2 = input("Enter a verb ")
+body_part6 = input("Enter body part ")
+adjective2 = input("Put in an adjective ")
+noun3 = input("Give a noun ")
+verb3 = input("Give me a verb ending in ing ")
+adjective3 = input("Give me another adjective ")
+body_part7 = input("Give another body part ")
+verb4 = input("Put in a verb ending in s ")
+level_of_intensity = input("Give me the level of intensity ")
+
+
+
+print("=======================================")
+print("Suddenly he grabs me,tipping me across his " + body_part1)
+print("With one " + adjective1 + " movement, he angles his " + noun1 + " so my " + body_part2 + " is resting on the " + noun2 + " beside him")
+print("He throws his right " + body_part3 + " over both mine and " + verb1 + " his left " + body_part4 + " on the small of my " + body_part5 + ".")
+print("holding me down so I cannot " + verb2 + ". . .")
+print("He places his " + body_part6 + " on my " + adjective2 + " " + noun3 + "," + "softly " + verb3 + " me," )
+print("stroking around and around with his " + adjective3 + " palm.")
+print("And then his "+ body_part7 + " is no longer there . . . and he")
+print(verb4 + " me-"+ level_of_intensity)
+
+
+
+
+
+
+
+
+
+
+
+
+
